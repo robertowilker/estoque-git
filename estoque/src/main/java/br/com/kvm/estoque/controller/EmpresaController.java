dc2a16010c4cc8a87f22fc8592a7b670db93a162
Roberto Wilker <robertowilker08@gmail.com>
2020-06-25 14:15:07 -0300

    Metodo detalhe criado, habilita o endpoint onde e possivel passar o id da empresa que retorna os detalhes da mesma.

--- estoque/src/main/java/br/com/kvm/estoque/controller/EmpresaController.java
+++ estoque/src/main/java/br/com/kvm/estoque/controller/EmpresaController.java
@@ -2,18 +2,21 @@ package br.com.kvm.estoque.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.kvm.estoque.controller.dto.EmpresaDetalhesDto;
 import br.com.kvm.estoque.controller.dto.EmpresaDto;
 import br.com.kvm.estoque.controller.form.EmpresaForm;
 import br.com.kvm.estoque.modelo.Empresa;
@@ -33,7 +36,7 @@ public class EmpresaController {
 	}
 
 	@PostMapping
-	public ResponseEntity<EmpresaDto> cadastrar(@RequestBody @Valid EmpresaForm form, UriComponentsBuilder uriBuilder) throws Exception{
+	public ResponseEntity<EmpresaDto> cadastrar(@RequestBody @Valid EmpresaForm form, UriComponentsBuilder uriBuilder){
 		Empresa empresa;
 
 		empresa = form.converter();
@@ -45,7 +48,14 @@ public class EmpresaController {
 
 	}
 
-//	@GetMapping("/{id}")
-//	public void detalhes(@PathVariable Long id) {
-//	}
+	@GetMapping("/{id}")
+	public ResponseEntity<EmpresaDetalhesDto> detalhes(@PathVariable Long id) {
+		Optional<Empresa> optional = empresaRepository.findById(id);
+		
+		if(optional.isPresent()) {
+			return ResponseEntity.ok(new EmpresaDetalhesDto(optional.get()));
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
